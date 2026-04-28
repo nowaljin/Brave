1d2f5ff480bba6746013ffe04dde43fad21d66f9
nowaljin <nowaljin13@gmail.com>
2026-04-28 12:20:30 +0900

    character moves update

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,30 +1,45 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rb;
+   private Animator anim;
+   private Rigidbody2D rb;
    [SerializeField] private float moveSpeed = 3.5f;
-   [SerializeField] private float jumpForce = 3.5f;
+   [SerializeField] private float jumpForce = 8f;
     private float xInput;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Update()
     {
         HandleInput();
         HandleMovement();
+        HandleAnimations();
 
 
     }
 
+    
 
-        private void HandleInput()
+    private void HandleAnimations()
+    {
+        bool isMoving = rb.linearVelocity.x != 0;
+
+        anim.SetBool("isMoving", isMoving);
+
+    }
+
+    private void HandleInput()
         {
             xInput= Input.GetAxisRaw("Horizontal");
+
+
             if (Input.GetKeyDown(KeyCode.Space))
                 Jump();
     }
