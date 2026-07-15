5d84ecb8b136fda5d9899aafe41a7309b30512c7
İlker Bozkurt <nowaljin13@gmail.com>
2026-07-15 17:59:36 +0900

    arrow speed change

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,22 +1,24 @@
 using System.Collections.Generic;
-rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Animator anim;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool canJump = true;
+    [SerializeField] private GameObject hitEffectPrefab;
 
     [Header("Attack details")]
     [SerializeField] private float attackRadius;
     [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask whatIsEnemy;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
 
     [Header("Ranged Attack")]
     [SerializeField] private GameObject arrowPrefab;
     [SerializeField] private Transform arrowSpawnPoint;
     [SerializeField] private float arrowSpeed = 10f;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    [SerializeField] private float arrowSpeedMultiplier = 0.5f;
 
     [Header("Movement details")]
     [SerializeField] private float moveSpeed = 3.5f;
@@ -24,18 +26,16 @@ public class Player : MonoBehaviour
     private float xInput;
     private bool facingRight = true;
     private bool canMove = true;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
 
     [Header("Collision details")]
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGrounded;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponentInChildren<Animator>();
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void Update()
     {
@@ -44,7 +44,7 @@ public class Player : MonoBehaviour
         HandleMovement();
         HandleAnimations();
         HandleFlip();
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void HandleInput()
     {
@@ -52,12 +52,12 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) TryToJump();
         if (Input.GetKeyDown(KeyCode.Mouse0)) TryToAttack();
         if (Input.GetKeyDown(KeyCode.Mouse1)) TryToShoot();
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void TryToAttack()
     {
         if (isGrounded) anim.SetTrigger("attack");
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void TryToShoot()
     {
@@ -66,11 +66,9 @@ public class Player : MonoBehaviour
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
             Vector2 shootDirection = (mousePos - arrowSpawnPoint.position).normalized;
-            rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
 
-            GameObject arrow = Instantiate(arrowPrefab, spawnPos, Quaternion.identity);
+            GameObject arrow = Instantiate(arrowPrefab, arrowSpawnPoint.position, Quaternion.identity);
             float angle = Mathf.Atan2(shootDirection.y, shootDirection.x) * Mathf.Rad2Deg;
-            rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
 
             Rigidbody2D rbArrow = arrow.GetComponent<Rigidbody2D>();
             if (rbArrow != null)
@@ -78,10 +76,11 @@ public class Player : MonoBehaviour
                 rbArrow.bodyType = RigidbodyType2D.Dynamic;
                 rbArrow.gravityScale = 0;
                 rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+            }
 
             Destroy(arrow, 5f);
         }
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     public void DamageEnemies()
     {
@@ -95,43 +94,47 @@ public class Player : MonoBehaviour
                 if (hitEffectPrefab != null) Instantiate(hitEffectPrefab, enemy.transform.position, Quaternion.identity);
             }
         }
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     public void EnableMovementAndJump(bool enable)
     {
         canMove = enable;
         canJump = enable;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void TryToJump()
     {
         if (isGrounded && canJump) rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(canMove ? xInput * moveSpeed : 0, rb.linearVelocity.y);
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    private void HandleCollision()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGrounded);
+        isGrounded = hit.collider != null;
+    }
 
     private void HandleAnimations()
     {
         anim.SetFloat("xVelocity", rb.linearVelocity.x);
         anim.SetFloat("yVelocity", rb.linearVelocity.y);
         anim.SetBool("isGrounded", isGrounded);
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void HandleFlip()
     {
         if ((rb.linearVelocity.x > 0 && !facingRight) || (rb.linearVelocity.x < 0 && facingRight)) Flip();
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void Flip()
     {
         transform.Rotate(0, 180, 0);
         facingRight = !facingRight;
-    rbArrow.linearVelocity = shootDirection * arrowSpeed * arrowSpeedMultiplier;
+    }
 
     private void OnDrawGizmosSelected()
     {
